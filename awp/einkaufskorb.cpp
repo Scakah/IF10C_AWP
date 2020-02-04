a976f37caacfd3de3894eb3e6fd47f24de677252
julian meier <Scakah@gmx.de>
2020-02-04 14:15:08 +0100

    Added einkaufswwagen

---
+++ awp/einkaufskorb.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+double d_gesamtbetragBerechnen(double d_gesamtbetrag, double d_preisDesPostens);
+
+int main () {
+	int i_anzahl = 0;
+	double d_preisDerWare, d_preisDesPostens, d_gesamtPreis = 0;
+	char c_weitereWareImWarenkorb;
+
+	cout << "Onlinehändler - Neuer Warenkorb wird angelegt";
+	do {
+		cout << "\nStückzahl: ";
+		cin >> i_anzahl;
+		cout<<"\nPreis der Ware: ";
+		cin >> d_preisDerWare;
+
+		d_preisDesPostens = d_preisDerWare * i_anzahl;
+
+		d_gesamtPreis = d_gesamtbetragBerechnen(d_gesamtPreis, d_preisDesPostens);
+
+		cout << "\nWeitere Artikel:";
+		cin >> c_weitereWareImWarenkorb;
+	}		
+	while  (c_weitereWareImWarenkorb);
+
+	cout << d_gesamtPreis;
+	getchar();	
+}
+
+double d_gesamtbetragBerechnen( double d_gesamtPreis, double d_preisDesPostens)
+{	
+	return d_gesamtPreis + d_preisDesPostens;
+}
